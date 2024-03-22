0b6e0478924a85da3fed6efb9402c96838858cb9
David Valle <52580367+dvallemo@users.noreply.github.com>
2024-03-22 09:49:32 -0400

    Update index.js

--- CounterDomProject/index.js
+++ CounterDomProject/index.js
@@ -1,23 +1,22 @@
-// document.getElementById("count").innerText = 5
-
-// let count = 5
-// count = count + 1
-// console.log(count)
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked (log it out)
-// change the count-el in the HTML to reflect the new count
 
 let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
 
 function increment() {
     console.log("clicked")
     count += 1
     console.log(count)
-    document.getElementById("count-el").innerHTML = count
+    countEl.textContent = count
 }
 
-increment()
+function save() {
+    saveEl.textContent += count + " - "
+    count = 0
+    countEl.textContent = count
+}
+
+
 
 
